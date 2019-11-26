90aef068990e66520bdf4842a56a586f1fbd9ba7
Ismail Mansuri <31936559+IsmailMansuri@users.noreply.github.com>
2019-11-26 10:26:50 -0500

    hi

--- Maze/src/main.cpp
+++ Maze/src/main.cpp
@@ -4,31 +4,36 @@
 
 //motor a
 
-#define in1 3
-#define in2 2
+#define m1a 3
+#define m1b 2
+
 //motor b
 
-#define in3 5
-#define in4 4
+#define m2a 5
+#define m2b 4
 
+int topSpeed = 200; //set the top speed for analog motor control (0-255)
+
+//front sensor
 int front_trig = 9; 
 int front_echo = 10; 
 
+//left sensor
 int left_trig = 6;
 int left_echo = 7;
 
+//right sensor
 int right_trig = 11;
 int right_echo = 12;
 
-void right();
-void Stop();
-void forward();
-void left();
-void back();
-//distance,time
+//initialize all functions
+void readRight(), readLeft(), readCenter(), right(), Stop(), forward(), left(), back(), turnAround();
+
+//initialize global variables
 long front_time, front_dis, right_time, right_dis, left_time, left_dis;
 
 void setup() {
+//declare pins for ultrasonic sensors
   pinMode(front_trig, OUTPUT); 
   pinMode(front_echo, INPUT);
 
@@ -37,15 +42,46 @@ void setup() {
 
   pinMode(right_trig, OUTPUT);
   pinMode(right_echo, INPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+
+  pinMode(m1a, OUTPUT);
+  pinMode(m1b, OUTPUT);
+  pinMode(m2a, OUTPUT);
+  pinMode(m2b, OUTPUT);
   
+  //start serial monitor
   Serial.begin(9600);
 }
 
 void loop() {
+  //print all the distances
+  Serial.print(right_dis);
+  Serial.print(":");
+  Serial.print(front_dis);
+  Serial.print(":");
+  Serial.println(left_dis);  
+
+  if (right_dis <= 6&& left_dis <= 6&& front_dis>5){ //if the left and right are walls, and if the front is open, go forward
+    forward(); 
+    Serial.println("Going Forward");
+  }
+  else if(right_dis > 6&&left_dis <= 6&& front_dis<=5){ //if the front and left are blocked, go right
+    right();
+    Serial.println("Turning Left");
+  }
+  else if (left_dis >6&&right_dis <= 6&& front_dis<=5){//if the right and front are blocked, turn left
+    left();
+    Serial.println("Turning Left");
+   }
+  else if (left_dis <=6&&right_dis <= 6&& front_dis <=6){//if all sides are blocked, turn around
+    back();
+    turnAround();
+    Serial.println("Turning Around");
+   }
+}
+
+
+void readRight() { //reads right ultrasonic sensor
+
   digitalWrite(front_trig, LOW);  
   delayMicroseconds(2);               
   digitalWrite(front_trig, HIGH); 
@@ -53,6 +89,11 @@ void loop() {
   digitalWrite(front_trig, LOW);  
   front_time = pulseIn(front_echo, HIGH); 
   front_dis = front_time/ 29 / 2;
+
+}
+
+void readLeft() { //reads left ultrasonic sensor
+
 
   digitalWrite(left_trig, LOW);
   delayMicroseconds(2);
@@ -62,6 +103,10 @@ void loop() {
   left_time= pulseIn(left_echo, HIGH);
   left_dis= left_time / 29 / 2;
 
+}
+
+void readCenter() { //reads center ultrasonic sensor
+
   digitalWrite(right_trig, LOW);
   delayMicroseconds(2);
   digitalWrite(right_trig, HIGH);
@@ -69,61 +114,60 @@ void loop() {
   digitalWrite(right_trig, LOW);
   right_time = pulseIn(right_echo, HIGH);
   right_dis = right_time / 29 / 2;
-  Serial.print(right_dis);
-  Serial.print(":");
-  Serial.print(front_dis);
-  Serial.print(":");
-  Serial.println(left_dis);
-  
-  
- if (right_dis <= 6&& left_dis <= 6&& front_dis>5){
-   forward();
- }
-  
-  else if(right_dis > 6&&left_dis <= 6&& front_dis>5){
-    right();
-    delay(100);
-  }
-
-  else if (left_dis >6&&right_dis <= 6&& front_dis>5){
-    left();
-  }
-
 
 }
-void right() {
+
+
+void right() { //turn right function
   
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
+  digitalWrite(m1a, LOW);
+  digitalWrite(m1b, LOW);
+  digitalWrite(m2a, HIGH);
+  digitalWrite(m2b, LOW);
+  
 }
-void Stop () {
+void Stop () { //Stop function
  
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-}
-void left() {
+  digitalWrite(m1a, LOW);
+  digitalWrite(m1b, LOW);
+  digitalWrite(m2a, LOW);
+  digitalWrite(m2b, LOW);
+  delay(500);
   
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
 }
-void forward() {
+void left() { //turn left function
   
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
+  digitalWrite(m1a, HIGH);
+  digitalWrite(m1b, LOW);
+  digitalWrite(m2a, LOW);
+  digitalWrite(m2b, LOW);
+ 
 }
-void back(){
+void forward() { //go forward function
 
-   
-  digitalWrite(in1, LOW);
-  digitalWrite(in2,HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
+  digitalWrite(m1a, HIGH);
+  digitalWrite(m1b, LOW);
+  digitalWrite(m2a, HIGH);
+  digitalWrite(m2b, LOW);
+
+}
+
+void back(){ //backup function
+
+  analogWrite(m1a, 0);
+  analogWrite(m1b,topSpeed);
+  analogWrite(m2a, 0);
+  analogWrite(m2b, topSpeed);
+  delay(500);
+
+}
+
+void turnAround(){ //turn around function
+
+  digitalWrite(m1a, LOW);
+  digitalWrite(m1b,HIGH);
+  digitalWrite(m2a, HIGH);
+  digitalWrite(m2b, LOW);
+  delay(500);
+
 }
